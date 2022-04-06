8d97fe7cc8f85969d252b8bb39cf1cf5e5a95104
Mithunkumar123 <mithun.selvaraj@gmail.com>
2022-04-07 01:07:24 +0530

    custom exception

--- PPMToll/src/main/java/com/udemy/api/services/ProjectService.java
+++ PPMToll/src/main/java/com/udemy/api/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.udemy.api.services;
 
 import com.udemy.api.domain.Project;
+import com.udemy.api.exceptions.ProjectIDException;
 import com.udemy.api.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception ex){
+            throw new ProjectIDException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already present in the system.");
+        }
+
     }
 
 }
