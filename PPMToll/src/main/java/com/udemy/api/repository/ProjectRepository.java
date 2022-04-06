96bec3011824cc9de00f38eb1c1f7fe0a61c481f
Mithunkumar123 <mithun.selvaraj@gmail.com>
2022-04-07 00:41:15 +0530

    till validation

---
+++ PPMToll/src/main/java/com/udemy/api/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.udemy.api.repository;
+
+import com.udemy.api.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> projects);
+}
---
+++ PPMToll/src/main/java/com/udemy/api/services/ProjectService.java
@@ -0,0 +1,18 @@
+package com.udemy.api.services;
+
+import com.udemy.api.domain.Project;
+import com.udemy.api.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+
+}
